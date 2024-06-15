1d6bb52edd593630cbbe0d62977a1436d49891ce
koudaihirata <2230051@ecc.ac.jp>
2024-06-15 12:37:58 +0900

    Initial Commit

---
+++ swift_tutorial/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swift_tutorial
+//
+//  Created by Kodai Hirata on 2024/06/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swift_tutorial/swift_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_tutorialApp.swift
+//  swift_tutorial
+//
+//  Created by Kodai Hirata on 2024/06/15.
+//
+
+import SwiftUI
+
+@main
+struct swift_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
